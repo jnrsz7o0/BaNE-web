a9ff2f28207e694fd309424801a6f3a42b6dc1ae
jnrsz7o0 <jnsrz7o0@protonmail.com>
2022-04-28 06:44:26 -0700

    added video

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import MainVideo from '../../assets/hexRotate.gif'
+import MainVideo from '../../assets/hexPulse.mp4'
 import './Video.css'
 
 const video = () => {
     return (
         <div className='video'>
-            <img src={MainVideo}></img>
+            <video autoPlay loop muted id='video'>
+                <source src={MainVideo} type='video/mp4'/>
+            </video>
+            <div className='video-text'>
+                <h1> <span className='green'>Ba</span>re-metal <span className='green'>N</span>ode <span className='green'>E</span>rector</h1>
+                <p><span className='green'>A Decentralized Oracle Network providing secure and simple Chainlink node management.</span></p>
+                <div className='btn-group'>
+                    <button className='btn'>button 1</button>
+                    <button className='btn btn-outline'>Documentation</button>
+                    <button className='btn'>button 3</button>
+                </div>
+            </div>
         </div>
     )
 }
