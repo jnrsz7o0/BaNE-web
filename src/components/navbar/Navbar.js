0aaeee0ad71cd89fc8b214e76a0f50618d369525
jnrsz7o0 <jnsrz7o0@protonmail.com>
2022-04-27 20:24:36 -0700

    added videos, initial webpage

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    return (
+        <div className="navbar">
+            <div className="container">
+                <h1 style={{marginLeft: '1rem', color:'#00FF33'}}>BaNE</h1>
+                <ul className={click ? 'nav active' : 'nav'}>
+                    <li className="nav-item">
+                        <a style={{color:'#00FF33'}} href="/">Service</a>
+                    </li>
+                    <li className="nav-item">
+                        <a style={{color:'#00FF33'}} href="/">Contract</a>
+                    </li>
+                    <li className="nav-item">
+                        <a style={{color:'#00FF33'}} href="/">Community</a>
+                    </li>
+                    <li className="nav-item">
+                        <a style={{color:'#00FF33'}} href="/">Docs</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className='btn' href="/">Apply</a>
+                    </li>
+                </ul>
+                <div onClick={handleClick} className="hamburger">
+                    {click ? (<AiOutlineClose className='icon' />) : (<AiOutlineMenu className='icon' />)}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/video/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainVideo from '../../assets/hexRotate.gif'
+import './Video.css'
+
+const video = () => {
+    return (
+        <div className='video'>
+            <img src={MainVideo}></img>
+        </div>
+    )
+}
+export default video
